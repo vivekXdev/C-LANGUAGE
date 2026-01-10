a2dc0251924350e52502c9df72eb6b87d99e651e
Vivek kumar <vk3052704@gmail.com>
2026-01-10 23:55:32 +0530

    dddd

--- 08_program.c
+++ 08_program.c
@@ -5,15 +5,14 @@ int main(void){
     printf("Enter the year:");
     scanf("%d",&year);
     
-    if(year%4 == 0)
-    printf("Yes %d is a leap year ",year);
-    else if(year%400 == 0)
-    printf("Yes %d is a leap year",year);
-    else if(year%100 == 0)
-    printf("%d is not a leap year",year);
-    else
-    printf("%d is not a leap year",year);
-
+   if(year % 400 == 0)
+    printf("%d is a leap year\n", year);
+else if(year % 100 == 0)     
+    printf("%d is not a leap year\n", year);
+else if(year % 4 == 0)       
+    printf("%d is a leap year\n", year);
+else
+    printf("%d is not a leap year\n", year);
 
     return 0;
 
---
+++ 09_program.c
@@ -0,0 +1,23 @@
+//WAP to calculate the area of triangle using hero's formula.
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float a,b,c,s,area;
+    printf("Enter the value of a:");
+    scanf("%f",&a);
+    printf("Enter the value of b:");
+    scanf("%f",&b);
+    printf("Enter the value of c:");
+    scanf("%f",&c);
+    
+    if (a + b > c && a + c > b && b + c > a){
+
+        s = (a + b + c)/2;
+        area = sqrt(s*(s-a)*(s-b)*(s-c));
+        printf("the area of triangle is %.2f\n",area);
+    }
+    else 
+
+    return 0;
+}
